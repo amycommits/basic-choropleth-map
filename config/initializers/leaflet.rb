5988f1e3f72cb2154f76a1e1af6ca1f49d12d3bc
Amy Detwiler <adetwiler@barquin.com>
2018-08-30 16:48:30 -0400

    why wasn't this committed before?

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,2 @@
+Leaflet.tile_layer = "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={pk.eyJ1IjoiYWRldHdpbGVyIiwiYSI6ImNqbGd3OHZ0ZTA1NjEzcXMzNHgyNnJ2b2oifQ.m2DaSb7JDExF0P3jKTkgMw}"
+Leaflet.max_zoom = 18
